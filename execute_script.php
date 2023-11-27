36f0695e0c3b98781f880f35bf51818ee21336a8
Rory Wood <rorywood9@live.com>
2023-11-26 20:17:39 -0500

    update

--- execute_script.php
+++ execute_script.php
@@ -1,7 +1,7 @@
 <?php
 // Execute the shell script
 
-$listFile = fopen("test.txt", "w") or die("Unable to open file");
+$listFile = fopen("output.txt", "w") or die("Unable to open file");
 $output = shell_exec('./html.sh');
-echo $output;
+fwrite($listFile, $output . "\n");
 ?>
