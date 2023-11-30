03a7510e181d3e607eb05d69a66ef8913df58e02
Rory Wood <rorywood9@live.com>
2023-11-29 23:12:33 -0500

    updated execute

--- execute_script.php
+++ execute_script.php
@@ -2,6 +2,6 @@
 //Execute the shell script
 
 $listFile = fopen("output.txt", "w") or die("Unable to open file");
-$output = shell_exec('firefox --headless --screenshot --window-size=800,480 todolist.html');
+$output = shell_exec('./html.sh');
 fwrite($listFile, $output . "\n");
 ?>
