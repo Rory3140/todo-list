a95f21ef6f27a38ad4f7b0c80b71755a28ced0b1
Rory Wood <rorywood9@live.com>
2024-01-08 00:14:38 +0000

    changed paths

--- scripts/updateDisplay.sh
+++ scripts/updateDisplay.sh
@@ -5,6 +5,6 @@ filename=./todolist.html
 
 firefox --headless --screenshot --window-size=800,480 file://$CWD/$filename > /dev/null 2>&1
 
-./displayImage.py screenshot.png > /dev/null 2>&1
+python3 scripts/displayImage.py screenshot.png > /dev/null 2>&1
 
 rm screenshot.png 
